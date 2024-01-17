13d4436d4fdc96b9c94974f2c495fe2df5cdf5a7
Arthur Emig <arthur.erich.emig@gmail.com>
2024-01-17 06:09:07 +0100

    added new url

--- WebSiteTimeAssist/myWebSiteTimeAssist/urls.py
+++ WebSiteTimeAssist/myWebSiteTimeAssist/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     # Other URL patterns
     path('delete_account/', views.delete_account, name='delete_account'),
+    path('user_profile/', views.user_profile, name="user_profile")
 ]
