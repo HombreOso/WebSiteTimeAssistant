8bbcf055314a85fad995857c61213770b27cf206
Arthur Emig <arthur.erich.emig@gmail.com>
2024-01-17 07:45:41 +0100

    typo corrected

--- WebSiteTimeAssist/WebSiteTimeAssist/urls.py
+++ WebSiteTimeAssist/WebSiteTimeAssist/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("user_operations/", include("myWebSiteAssist.urls"))
+    path("user_operations/", include("myWebSiteTimeAssist.urls"))
 ]
