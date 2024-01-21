cbb860c3572a911f0b593ef88040e7062ec5b498
Arthur Emig <arthur.erich.emig@gmail.com>
2024-01-21 14:45:37 +0100

    delete account function invoked

--- WebSiteTimeAssist/myWebSiteTimeAssist/urls.py
+++ WebSiteTimeAssist/myWebSiteTimeAssist/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     # Other URL patterns
     path('delete_account/', views.delete_account_rendering, name='delete_account'),
+    path('delete_account_decided/', views.delete_account, name='delete_account_decided'),
     path('user_profile/', views.user_profile, name="user_profile"),
     path('home/', views.home_rendering, name="home")
 ]
