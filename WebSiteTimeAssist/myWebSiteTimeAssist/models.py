57849c8e6b897eac712401c2bb51a0fe14da6e35
Arthur Emig <arthur.erich.emig@gmail.com>
2024-01-21 13:49:33 +0100

    user credentials model

--- WebSiteTimeAssist/myWebSiteTimeAssist/models.py
+++ WebSiteTimeAssist/myWebSiteTimeAssist/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import CharField
 
 # Create your models here.
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class UserCredentials(models.Model):
+    nickname = CharField(max_length=100)
+    password = CharField(max_length=100)
